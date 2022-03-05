13e2ba4516eb66b47dde20f2e3fcea485d18b413
yassin <yassinzaherelfeliri@gmail.com>
2022-03-05 12:15:20 +0100

    initial commit

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./homepage.styles.scss";
+import DirectoryItem from "../../components/directory-menu/directory-item.component";
+
+const HomePage = () => (
+    <div className="homepage">
+            <DirectoryItem/>
+    </div>
+ );
+export default HomePage;
